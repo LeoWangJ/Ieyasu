e44c1d60d29c8cdb68b94a46df4fcb38e3d955d3
leowang <rfv7855659@gmail.com>
2022-08-20 15:12:15 +0800

    ✨add depoly gh-pages

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ const ComponentsPlugin = require('unplugin-vue-components/webpack')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '.',
   pluginOptions: {
     windicss: {
       // see https://github.com/windicss/vite-plugin-windicss/blob/main/packages/plugin-utils/src/options.ts
